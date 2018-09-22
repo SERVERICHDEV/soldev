07f2cea06def24e6e828dbe77670af3c3d909e4f
iamwebmaster <iamwebmaster2@gmail.com>
2018-09-23 00:53:41 +0700

    xddd

--- deploy.php
+++ deploy.php
@@ -2,10 +2,10 @@
 $dt = date("Y-m-d H:i:s");
 include "includes/function_send.php";
 
-$git_version_tmp = shell_exec('/usr/bin/gitgit --version');
+$git_version_tmp = shell_exec('/usr/bin/git --version');
 $git_version = htmlentities(trim($git_version_tmp)) . "\n";
 
-$git_status_tmp = shell_exec('/usr/bin/gitgit status');
+$git_status_tmp = shell_exec('/usr/bin/git status');
 $git_status = htmlentities(trim($git_status_tmp)) . "\n";
 
 $git_test_tmp =  system('/usr/bin/git config --list');
@@ -14,13 +14,17 @@ $git_test = htmlentities(trim($git_test_tmp)) . "\n";
 $git_pull_tmp =  system('/usr/bin/git pull');
 $git_pull = htmlentities(trim($git_pull_tmp)) . "\n";
 
-$git_log_tmp =  system('/usr/bin/git git log -1');
+$git_log_tmp =  system('/usr/bin/git log -1');
 $git_log = htmlentities(trim($git_log_tmp)) . "\n";
 
 
 srai('C54ec3f5f67fd4be664d9349e4bbdf5e0',"Git Update
 $dt on $git_version
+==================
+$git_status
+..................
 $git_pull
+..................
 $git_log
 ");
 ?>
