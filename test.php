efab49d42584bece2a8ca7ba6aca50bc78aeef10
iamwebmaster <iamwebmaster2@gmail.com>
2018-09-23 00:20:42 +0700

    s

--- test.php
+++ test.php
@@ -1,6 +1,6 @@
 <?
 $dt = date("Y-m-d H:i:s");
-include_once "includes/function_send.php";
+include "includes/function_send.php";
 
 // $git_version_tmp = shell_exec('git --version');
 // $git_version = htmlentities(trim($git_version_tmp)) . "\n";
@@ -17,15 +17,5 @@ include_once "includes/function_send.php";
 // $git_pull_tmp =  system('/usr/bin/git pull');
 // $git_pull = htmlentities(trim($git_pull_tmp)) . "\n";
 
-srai('C54ec3f5f67fd4be664d9349e4bbdf5e0',"Git Update
-$dt
-$git_version
-$git_status
-
-$git_pull
-..........
-$git_test
-.........
-$git_pull
-");
+srai('C54ec3f5f67fd4be664d9349e4bbdf5e0',"Git Updat $dt");
 ?>
