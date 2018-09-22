dfa22211fdccceeaa79cd3c699e5cd5d6226703e
iamwebmaster <iamwebmaster2@gmail.com>
2018-09-23 00:39:49 +0700

    set git hook

--- test.php
+++ test.php
@@ -2,14 +2,11 @@
 $dt = date("Y-m-d H:i:s");
 include "includes/function_send.php";
 
-$git_version_tmp = shell_exec('git --version');
+$git_version_tmp = shell_exec('/usr/bin/gitgit --version');
 $git_version = htmlentities(trim($git_version_tmp)) . "\n";
 
-$git_status_tmp = shell_exec('git status');
+$git_status_tmp = shell_exec('/usr/bin/gitgit status');
 $git_status = htmlentities(trim($git_status_tmp)) . "\n";
-
-$git_pull_tmp = shell_exec('git pull');
-$git_pull = htmlentities(trim($git_pull_tmp)) . "\n";
 
 $git_test_tmp =  system('/usr/bin/git config --list');
 $git_test = htmlentities(trim($git_test_tmp)) . "\n";
@@ -17,8 +14,13 @@ $git_test = htmlentities(trim($git_test_tmp)) . "\n";
 $git_pull_tmp =  system('/usr/bin/git pull');
 $git_pull = htmlentities(trim($git_pull_tmp)) . "\n";
 
-srai('C54ec3f5f67fd4be664d9349e4bbdf5e0',"Git Updat
-$dt
+$git_log_tmp =  system('/usr/bin/git git log -1');
+$git_log = htmlentities(trim($git_log_tmp)) . "\n";
+
+
+srai('C54ec3f5f67fd4be664d9349e4bbdf5e0',"Git Update
+$dt on $git_version
 $git_pull
+$git_log
 ");
 ?>
