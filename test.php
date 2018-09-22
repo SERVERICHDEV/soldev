6bf20dd47a823fc0400d3e6a1a40ed9142fe58df
iamwebmaster <iamwebmaster2@gmail.com>
2018-09-23 00:31:10 +0700

    c

--- test.php
+++ test.php
@@ -14,8 +14,9 @@ include "includes/function_send.php";
 // $git_test_tmp =  system('/usr/bin/git config --list');
 // $git_test = htmlentities(trim($git_test_tmp)) . "\n";
 //
-// $git_pull_tmp =  system('/usr/bin/git pull');
-// $git_pull = htmlentities(trim($git_pull_tmp)) . "\n";
+$git_pull_tmp =  system('/usr/bin/git pull');
+$git_pull = htmlentities(trim($git_pull_tmp)) . "\n";
 
-srai('C54ec3f5f67fd4be664d9349e4bbdf5e0',"Git Updat $dt");
+srai('C54ec3f5f67fd4be664d9349e4bbdf5e0',"Git Updat $dt
+$git_pull");
 ?>
