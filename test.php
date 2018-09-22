414697bc28ca8e54c22ef76affdf5f6d0c796e4e
iamwebmaster <iamwebmaster2@gmail.com>
2018-09-22 22:36:22 +0700

    แแ

--- test.php
+++ test.php
@@ -1,7 +1,4 @@
 <?
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
 include_once "includes/function_send.php";
-srai('C54ec3f5f67fd4be664d9349e4bbdf5e0',"$l : $start_nf / $num_all | $n | DEL : $nd |  $execution_time");
+srai('C54ec3f5f67fd4be664d9349e4bbdf5e0',"มีคนอัพเดทไฟล์จร้า");
 ?>
