d7529005c3eb3cab4c578fc8b0df0ad169888042
iamwebmaster <iamwebmaster2@gmail.com>
2018-09-22 22:49:34 +0700

    ddddd

--- test.php
+++ test.php
@@ -8,7 +8,13 @@ $git_version  = htmlentities(trim($git_version_tmp)) . "\n";
 $git_status_tmp = shell_exec('git status');
 $git_status = htmlentities(trim($git_status_tmp)) . "\n";
 
+$git_pull_tmp = shell_exec('git pull');
+$git_pull = htmlentities(trim($git_pull_tmp)) . "\n";
+
 srai('C54ec3f5f67fd4be664d9349e4bbdf5e0',"Git Update $dt
 $git_version
-$git_status");
+$git_status
+
+
+$git_pull");
 ?>
